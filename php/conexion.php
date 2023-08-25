cb1dd251a840e70af8707ecd0e34be4d66b1838d
GuascaC <109239941+GuascaC@users.noreply.github.com>
2023-08-25 07:43:05 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,20 @@
 <?php
-    $con = mysqli_connect('localhost','root','','treezone');
-    if ($con){
+class Connection extends PDO{
+    private $DB_type = 'mysql';
+    private $host = 'localhost';
+    private $DB_name = 'treezone';
+    private $DB_user = 'root';
+    private $password = '';
+    public function __construct(){
+            $this->connection = mysqli_connect($this->host, $this->DB_user, $this->password, $this->DB_name);
+            if (!$this->connection) {
+                die("Ha surgido un error y no se puede conectar a la base de datos. Detalle: " . mysqli_connect_error());
+        }
     }
-    else{
-        die("No se puedo conectar");
+    public function getConnection() {
+        return $this->connection;
     }
+}
+$obj= new Connection();
+$cone= $obj->getConnection();;
 ?>
